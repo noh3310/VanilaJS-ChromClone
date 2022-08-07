d7711c03c90af17c213c2fccc5e621cbad1c25ae
GunHo <noh3310@gmail.com>
2022-08-08 00:01:23 +0900

    add Clock

---
+++ js/clock.js
@@ -0,0 +1,15 @@
+const clock = document.querySelector("#clock");
+
+clock.innerText = "hihihihihi";
+
+function getClock() {
+  const date = new Date();
+  const hour = date.getHours().toString().padStart(2, "0");
+  const minute = date.getMinutes().toString().padStart(2, "0");
+  const second = date.getSeconds().toString().padStart(2, "0");
+  clock.innerText = `${hour}:${minute}:${second}`;
+}
+
+getClock();
+// Execute function after interval
+setInterval(getClock, 1000);
